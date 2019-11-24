d5700369971b3bdda4be2e48d5e8300fbc4c2fd2
shinyahashimoto <shinya.hashimoto@rakus-partners.co.jp>
2019-11-24 14:18:58 +0900

    ページング機能を追加しました（まだ未実装、htmlの改修必須）

--- src/main/java/com/example/controller/ShowItemListController.java
+++ src/main/java/com/example/controller/ShowItemListController.java
@@ -1,14 +1,17 @@
 package com.example.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Item;
 import com.example.service.ItemService;
+
 
 /**
  * 全商品情報を表示します.
@@ -22,6 +25,8 @@ public class ShowItemListController {
 
 	@Autowired
 	private ItemService service;
+	
+	private static final int VIEW_SIZE = 10;
 
 	/**
 	 * 全商品情報を表示します.
@@ -30,10 +35,50 @@ public class ShowItemListController {
 	 * @return 全商品情報
 	 */
 	@RequestMapping("/show-item-list")
-	public String showItemList(Model model) {
+	public String showItemList(Model model, Integer page) {
+		
+		if(page == null) {
+			//ページ数の指定がない場合は１ページ目を表示させる
+			page = 1;
+		}
+		
 		List<Item> itemList = service.showList();
 		model.addAttribute("itemList", itemList);
+		
+		// 表示させたいページ数、ページサイズ、従業員リストを渡し１ページに表示させる従業員リストを絞り込み
+		Page<Item> itemPage = service.showListPaging(page, VIEW_SIZE, itemList);
+		model.addAttribute("itemPage", itemPage);
+
+		// ページングのリンクに使うページ数をスコープに格納 (例)28件あり1ページにつき10件表示させる場合→1,2,3がpageNumbersに入る
+		List<Integer> pageNumbers = calcPageNumbers(model, itemPage);
+		model.addAttribute("pageNumbers", pageNumbers);		
 
 		return "item_list";
 	}
+	
+	/**
+	 * ページングのリンクに使うページ数をスコープに格納 (例)28件あり1ページにつき10件表示させる場合→1,2,3がpageNumbersに入る
+	 * 
+	 * @param model        モデル
+	 * @param itemPage ページング情報
+	 */
+	private List<Integer> calcPageNumbers(Model model, Page<Item> itemPage) {
+		int totalPages = itemPage.getTotalPages();
+		List<Integer> pageNumbers = null;
+		if (totalPages > 0) {
+			pageNumbers = new ArrayList<Integer>();
+			for (int i = 1; i <= totalPages; i++) {
+				pageNumbers.add(i);
+			}
+		}
+		return pageNumbers;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -1,13 +1,18 @@
 package com.example.service;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.Item;
 import com.example.repository.ItemRepository;
+
 
 /**
  * 商品情報を操作するサービスクラス.
@@ -39,6 +44,36 @@ public class ItemService {
 	 */
 	public List<Item> showList() {
 		return repository.findAll();
+	}
+	
+	/**
+	 * ページング用メソッド.
+	 * @param page 表示させたいページ数
+	 * @param size １ページに表示させる従業員数
+	 * @param itemList 絞り込み対象リスト
+	 * @return １ページに表示されるサイズ分の従業員一覧情報
+	 */
+	public Page<Item> showListPaging(int page, int size, List<Item> itemList) {
+	    // 表示させたいページ数を-1しなければうまく動かない
+	    page--;
+	    // どの従業員から表示させるかと言うカウント値
+	    int startItemCount = page * size;
+	    // 絞り込んだ後の従業員リストが入る変数
+	    List<Item> list;
+
+	    if (itemList.size() < startItemCount) {
+	    	// (ありえないが)もし表示させたい従業員カウントがサイズよりも大きい場合は空のリストを返す
+	        list = Collections.emptyList();
+	    } else {
+	    	// 該当ページに表示させる従業員一覧を作成
+	        int toIndex = Math.min(startItemCount + size, itemList.size());
+	        list = itemList.subList(startItemCount, toIndex);
+	    }
+
+	    // 上記で作成した該当ページに表示させる従業員一覧をページングできる形に変換して返す
+	    Page<Item> itemPage
+	      = new PageImpl<Item>(list, PageRequest.of(page, size), itemList.size());
+	    return itemPage;
 	}
 
 }
