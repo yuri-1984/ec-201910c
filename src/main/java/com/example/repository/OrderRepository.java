428fc6ad11dcf425bdd20c65440f80208710f8f5
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2019-11-26 15:07:20 +0900

    orderRepository メソッド作成しました

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.example.repository;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,8 @@ public class OrderRepository {
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
-	public static final ResultSetExtractor<Order> ORDER_EXTRACTOR = (rs) -> {
+	public static final ResultSetExtractor<List<Order>> ORDER_EXTRACTOR = (rs) -> {
+		List<Order> orderList = new LinkedList<Order>();
 		int preId = 0;
 		Order order = null;
 		List<OrderItem> orderItemList = null;
@@ -93,30 +95,65 @@ public class OrderRepository {
 				item.setImagePath(rs.getString("i_image_path"));
 
 			}
+			preId = order.getId();
 
 		}
-		return order;
+		return orderList;
 
 	};
-
 	/**
 	 * 注文内容を検索する.
 	 * 
-	 * @param userId
-	 * @param status
-	 * @return 注文内容
+	 * @param userId 値が無い場合はsessionIdを格納する.
+	 * @param status 注文前なので０を入れる.
+	 * @return 注文内容 Orderオブジェクト.
 	 */
-	public Order findByUserIdAndStatus(Integer userId, Integer status) {
+	public Order findByUserId(Integer userId) {
 
-		String sql = "select id,user_id,status,total_price,order_date,destination_name,destination_email,destination_zipcode,destination_address,destination_tell,delivery_time,payment_method from orders where user_id=:userId && status=:status";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId);
+		String sql = "SELECT o.id o_id,o.user_id o_user_id,o.status o_status,o.total_price o_total_price,o.order_date o_order_date,o.destination_name o_destination_name,o.destination_email o_destination_email,o.destination_zipcode o_destination_zipcode,o.destination_address o_destination_address,o.destination_tel o_destination_tel,o.delivery_time o_delivery_time,o.payment_method o_payment_method,"
+				+ "oi.id oi_id, oi.order_id oi_order_id,oi.quantity oi_quantity, oi.item_id oi_item_id, oi.size oi_size, ot.id ot_id, ot.order_item_id ot_order_item_id ,ot.topping_id ot_topping_id,t.id t_id, t.name t_name, t.price_m t_price_m,t.price_l t_price_l, i.id i_id,i.name i_name, i.description i_description, i.price_m i_price_m, i.price_l i_price_l,i.image_path i_image_path, i.deleted i_deleted"
+				+ "FROM orders o" + "LEFT OUTER JOIN order_items oi on o.id = oi.order_id"
+				+ "LEFT OUTER JOIN order_toppings ot on oi.id = ot.order_item_id"
+				+ "LEFT OUTER JOIN items i on i.id = oi.item_id"
+				+ "LEFT OUTER JOIN toppings t on t.id = ot.topping_id ";
 
-		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId).addValue("status", status);
-
-		Order order = template.query(sql, param, ORDER_EXTRACTOR);
-
-		return order;
+		List<Order> orderList = template.query(sql, param, ORDER_EXTRACTOR);
+		System.out.println(orderList.get(0));
+		return orderList.get(0);
 
 	}
-	
-	
+	/**
+	 * 注文内容検索する.
+	 * 
+	 * @return 注文内容
+	 */
+	public List<Order> findAll() {
+		String sql = "SELECT o.id o_id,o.user_id o_user_id,o.status o_status,o.total_price o_total_price,o.order_date o_order_date,o.destination_name o_destination_name,o.destination_email o_destination_email,o.destination_zipcode o_destination_zipcode,o.destination_address o_destination_address,o.destination_tel o_destination_tel,o.delivery_time o_delivery_time,o.payment_method o_payment_method,"
+				+ "oi.id oi_id, oi.order_id oi_order_id,oi.quantity oi_quantity, oi.item_id oi_item_id, oi.size oi_size, ot.id ot_id, ot.order_item_id ot_order_item_id ,ot.topping_id ot_topping_id,t.id t_id, t.name t_name, t.price_m t_price_m,t.price_l t_price_l, i.id i_id,i.name i_name, i.description i_description, i.price_m i_price_m, i.price_l i_price_l,i.image_path i_image_path, i.deleted i_deleted"
+				+ " FROM orders o" + " LEFT OUTER JOIN order_items oi on o.id = oi.order_id"
+				+ " LEFT OUTER JOIN order_toppings ot on oi.id = ot.order_item_id"
+				+ " LEFT OUTER JOIN items i on i.id = oi.item_id"
+				+ " LEFT OUTER JOIN toppings t on t.id = ot.topping_id ";
+		List<Order> orderList = template.query(sql, ORDER_EXTRACTOR);
+		return orderList;
+
+	}
+	/**
+	 * 注文内容とユーザ情報を登録する.
+	 * 
+	 * @param order リクエストパラメーター.
+	 * 
+	 * @return 注文内容とユーザ情報を登録する.
+	 */
+	public void insert(Order order) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
+		String sql = "INSERT INTO orders(user_id,status,total_price,order_date,destination_name,destination_email,destination_zipcode,destination_address,destination_tel,delivery_time,payment_method)"
+				+ " Values(:user_id, :status, :total_price, :order_date, :destination_name, :destination_email, :destination_zipcode, :destination_address, :destination_tel, :delivery_time, :payment_method)";
+		if (order.getId() != null) {
+			throw new NullPointerException();
+		}
+		template.update(sql, param);
+	}
+
 }
