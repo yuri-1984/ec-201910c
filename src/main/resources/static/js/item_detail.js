a326a96c730558e854e979f6cf7302e5222c2568
yuichiyasui <yassan.b.a.w@gmail.com>
2019-11-27 17:46:48 +0900

    途中経過アップ

--- src/main/resources/static/js/item_detail.js
+++ src/main/resources/static/js/item_detail.js
@@ -1,4 +1,12 @@
 $(function(){
+	var total_price = 0;
+//	$("#total_price").text(total_price);
+	
+	$('input:checkbox').change(function() {
+        var topping_count = $('.col-sm-12 input:checkbox:checked').length;
+        var item_priceM
+        $("#total_price").text(topping_count * 200);
+    }).trigger('change');
 	
 	
 	//	var total_price = parseInt($("input[name='responsibleCompany']:radio").val());;
@@ -12,10 +20,7 @@ $(function(){
 //			var check_count = $("input[type='checkbox']:checked").length;
 //			alert(check_count * 200);
 //		})
-
-	$function calc_price(){
-		
-		
-	};
+//	$function calc_price(){
+//	};
 
 });
