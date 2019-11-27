9f8954cb536a48af71c4c88a5e700d078f1150ae
yuichiyasui <yassan.b.a.w@gmail.com>
2019-11-27 15:23:26 +0900

    item_detail.js作成中 基本実装動作確認のため一旦プッシュ

--- src/main/resources/static/js/item_detail.js
+++ src/main/resources/static/js/item_detail.js
@@ -1,3 +1,13 @@
 $(function(){
-	
+	var total_price = parseInt($("input[name='responsibleCompany']:radio").val());;
+	$("#total_price").text(total_price);
+		$("input[name='responsibleCompany']:radio").change(function(){
+			total_price = parseInt($(this).val());			
+			$("#total_price").text(total_price);
+		});
+		
+		$("input[type='checkbox']").change(function(){
+			var check_count = $("input[type='checkbox']:checked").length;
+			alert(check_count * 200);
+		})
 });
