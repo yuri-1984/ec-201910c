bc9ae843890810645d832ace7972209f71fe427e
yuichiyasui <yassan.b.a.w@gmail.com>
2019-11-27 15:53:37 +0900

    item_detail.js更新

--- src/main/resources/static/js/item_detail.js
+++ src/main/resources/static/js/item_detail.js
@@ -1,13 +1,21 @@
 $(function(){
-	var total_price = parseInt($("input[name='responsibleCompany']:radio").val());;
-	$("#total_price").text(total_price);
-		$("input[name='responsibleCompany']:radio").change(function(){
-			total_price = parseInt($(this).val());			
-			$("#total_price").text(total_price);
-		});
+	
+	
+	//	var total_price = parseInt($("input[name='responsibleCompany']:radio").val());;
+//	$("#total_price").text(total_price);
+//		$("input[name='responsibleCompany']:radio").change(function(){
+//			total_price = parseInt($(this).val());			
+//			$("#total_price").text(total_price);
+//		});
+//		
+//		$("input[type='checkbox']").change(function(){
+//			var check_count = $("input[type='checkbox']:checked").length;
+//			alert(check_count * 200);
+//		})
+
+	$function calc_price(){
 		
-		$("input[type='checkbox']").change(function(){
-			var check_count = $("input[type='checkbox']:checked").length;
-			alert(check_count * 200);
-		})
+		
+	};
+
 });
