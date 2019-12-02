9b6ffbbebccefaa9de644508da1e68c51749eb7d
shinyahashimoto <shinya.hashimoto@rakus-partners.co.jp>
2019-12-02 14:11:55 +0900

    ログイン成功のバグ、ログイン後のパス、ログアウト後のパスを修正しました

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Credit;
+import com.example.domain.LoginUser;
 import com.example.domain.Order;
 import com.example.domain.ReceiveCredit;
 import com.example.form.OrderForm;
@@ -43,8 +45,9 @@ public class OrderController {
 	 * @return 注文確認画面
 	 */
 	@RequestMapping("/showOrderConfirm")
-	public String showOrderConfirm(Integer userId, Model model) {
+	public String showOrderConfirm(@AuthenticationPrincipal LoginUser loginUser, Model model) {
 		//カートの中身を表示する
+		int userId = loginUser.getUser().getId();
 		Order order = shoppingCartService.showCartList(userId);
 		model.addAttribute("order", order);
 		return "order_confirm";
@@ -68,13 +71,14 @@ public class OrderController {
 	public String order(
 			@Validated 
 			OrderForm form,
+			@AuthenticationPrincipal LoginUser loginUser,
 			BindingResult result,
 			Model model) {
 		System.err.println("OrderControllerの中身"+form);
 		if (result.hasErrors()) {
 			System.err.println("バリデーションエラー出すよ");
 			System.err.println(result);
-			return showOrderConfirm(form.getUserId(),model);
+			return showOrderConfirm(loginUser,model);
 		} 
 		if (Integer.parseInt(form.getPaymentMethod())==2) {
 			Credit credit = new Credit();
@@ -100,7 +104,7 @@ public class OrderController {
 			// falseだったらエラーを追加して注文確認画面に戻る
 			} else if (checkService.service(credit, receiveCredit) == false) {
 				result.rejectValue("error", null, "クレジットカード情報が不正です");
-				return showOrderConfirm(form.getUserId(),model);
+				return showOrderConfirm(loginUser,model);
 			}
 		}
 		Order order = orderService.registerOrder(form);
